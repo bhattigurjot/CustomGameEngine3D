918e5dd715703f28fbb6e4493de32fe319a8758d
bhattigurjot <bhattigurjot@gmail.com>
2018-06-26 16:26:29 -0600

    Added stb_image.h file and updated Logger file

--- CustomGameEngine3D/RenderEngine/Utils/Logger.h
+++ CustomGameEngine3D/RenderEngine/Utils/Logger.h
@@ -7,3 +7,8 @@ static void LogError(const std::string& _errorMessage, const char* _sdlError)
 {
 	std::cerr << "Error: " << _errorMessage << _sdlError << std::endl;
 }
+
+static void LogError(const std::string& _errorMessage)
+{
+	std::cerr << "Error: " << _errorMessage << std::endl;
+}
